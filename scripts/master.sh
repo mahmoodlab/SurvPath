9ec23665ee1c2d2be2ed3790f5da0105ef2f1783
Guillaume Jaume <guillaume.jaume@epfl.ch>
2024-03-18 15:01:32 -0400

    include additional scripts

---
+++ scripts/master.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source scripts/abmil.sh 
+source scripts/deepmisl.sh 
+source scripts/transmil.sh 
+source scripts/mcat_pathways.sh 
+# source scripts/survpath.sh 
---
+++ scripts/mcat_motcat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DATA_ROOT_DIR='/media/ssd/survpath/' # where are the TCGA features stored?
+BASE_DIR="/home/guillaume/Documents/multimodal/SurvPath" # where is the repo cloned?
+TYPE_OF_PATH="combine" # what type of pathways? 
+MODEL="coattn_motcat" # what type of model do you want to train?
+FUSION="concat" # what type of fusion do you want to do?
+STUDIES=("brca")
+
+for STUDY in ${STUDIES[@]};
+do
+
+    CUDA_VISIBLE_DEVICES=2 python main.py \
+        --study tcga_${STUDY} --task survival --split_dir splits --which_splits 5foldcv \
+        --type_of_path $TYPE_OF_PATH --modality $MODEL --data_root_dir $DATA_ROOT_DIR/$STUDY/uni_features/ --label_file datasets_csv/metadata/tcga_${STUDY}.csv \
+        --omics_dir datasets_csv/raw_rna_data/${TYPE_OF_PATH}/${STUDY} --results_dir results_${STUDY} \
+        --batch_size 1 --lr 0.0005 --opt radam --reg 0.0001 \
+        --alpha_surv 0.5 --weighted_sample --max_epochs 5 --encoding_dim 1024 \
+        --label_col survival_months_dss --k 5 --bag_loss nll_surv --n_classes 4 --num_patches 4096 --wsi_projection_dim 256 --fusion $FUSION
+
+done 
