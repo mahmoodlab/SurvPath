3f73ddd6705ec67d643020c5bb04fb13f9f382cc
Anurag Vaidya <55669017+ajv012@users.noreply.github.com>
2024-11-19 06:50:18 -0500

    Update process_args.py

--- utils/process_args.py
+++ utils/process_args.py
@@ -42,7 +42,7 @@ def _process_args():
     parser.add_argument('--which_splits', type=str, default="10foldcv", help='where are splits')
         
     #----> training related 
-    parser.add_argument('--max_epochs', type=int, default=20, help='maximum number of epochs to train (default: 200)')
+    parser.add_argument('--max_epochs', type=int, default=20, help='maximum number of epochs to train (default: 20)')
     parser.add_argument('--lr', type=float, default=1e-4, help='learning rate (default: 0.0001)')
     parser.add_argument('--seed', type=int, default=1, help='random seed for reproducible experiment (default: 1)')
     parser.add_argument('--opt', type=str, default="adam", help="Optimizer")
